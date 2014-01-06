ee45de28b9409e89b2e8b457cb8f99559da382e1
ongaeshi <ongaeshi0621@gmail.com>
2014-01-07 02:58:56 +0900

    Add index column
    Can’t get real record.id from search result.
    Maybe need GrnMini::Hash.

--- sample/mini-directory-search.rb
+++ sample/mini-directory-search.rb
@@ -8,13 +8,15 @@ module Input
   module_function
   
   def from_dir(array, dir = ".")
+    index = 1
     puts "Create database .."
     Find.find(File.expand_path(dir)) do |filename|
       Find.prune if ignore_dir?(filename)
 
       if File.file? filename
         next if ignore_file?(filename)
-        array << {filename: filename, text: read_file(filename), timestamp: File.stat(filename).mtime, suffix: File.extname(filename).sub('.', "") }
+        array << {filename: filename, text: read_file(filename), timestamp: File.stat(filename).mtime, suffix: File.extname(filename).sub('.', ""), index: index } # index is dirty
+        index += 1
       end
     end
     puts "Input complete : #{array.size} files"
@@ -56,7 +58,7 @@ class Search
       elements = []
 
       page_entries.each do |record|
-        element = "<hr><a href=\"/#{record.id}\">#{record.filename}</a>\n"
+        element = "<hr><a href=\"/#{record.index}\">#{record.filename}</a>\n"
         
         snippet.execute(record.text).each do |segment|
           element += "<pre style=\"border:1px solid #bbb;\">#{segment}</pre>\n"

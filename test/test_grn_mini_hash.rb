a4bfac39047ecef1aec17e733d258fbf5534c242
ongaeshi <ongaeshi0621@gmail.com>
2014-01-09 01:10:38 +0900

    Add TestGrnMiniHash#test_select, test_select2

--- test/test_grn_mini_hash.rb
+++ test/test_grn_mini_hash.rb
@@ -18,4 +18,34 @@ class TestGrnMiniHash < MiniTest::Unit::TestCase
     end 
   end
 
+  def test_select
+    GrnMini::Hash.tmpdb do |hash|
+      hash["a"] = {text:"aaa", number:1}
+      hash["b"] = {text:"bbb", number:2}
+      hash["c"] = {text:"ccc", number:3}
+
+      results = hash.select("bb")
+
+      assert_equal 1, results.size
+      assert_equal "b", results.first.key.key
+      assert_equal "bbb", results.first.text
+    end 
+  end
+
+  def test_select2
+    GrnMini::Hash.tmpdb do |hash|
+      hash["a"] = {text:"aaa", number:1}
+      hash["b"] = {text:"bbb", number:2}
+      hash["c"] = {text:"bbb", number:20}
+      hash["d"] = {text:"ccc", number:3}
+
+      results = hash.select("bb number:<10")
+
+      assert_equal 1, results.size
+      assert_equal "b", results.first.key.key
+      assert_equal "bbb", results.first.text
+      assert_equal 2, results.first.number
+    end 
+  end
+
 end

21fcc7273650ec59e68337fa27172f14204a86f8
ongaeshi <ongaeshi0621@gmail.com>
2014-01-12 23:38:50 +0900

    Add GrnMini::create_or_open, tmpdb

--- lib/grn_mini.rb
+++ lib/grn_mini.rb
@@ -2,7 +2,24 @@ require "grn_mini/version"
 require "grn_mini/array"
 require "grn_mini/hash"
 require "grn_mini/util"
+require 'groonga'
+require 'tmpdir'
 
 module GrnMini
-  # Your code goes here...
+  module_function
+
+  def create_or_open(path)
+    unless File.exist?(path)
+      Groonga::Database.create(path: path)
+    else
+      Groonga::Database.open(path)
+    end
+  end
+
+  def tmpdb
+    Dir.mktmpdir do |dir|
+      create_or_open(File.join(dir, "tmp.db"))
+      yield
+    end
+  end
 end

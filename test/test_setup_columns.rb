92b0d797d1d5e31e98e39554380a3e1d336bee88
ongaeshi <ongaeshi0621@gmail.com>
2014-02-11 18:10:02 +0900

    Add test_columns, test_multi_table_columns
    Test column names if they were generated correctly

--- test/test_setup_columns.rb
+++ test/test_setup_columns.rb
@@ -73,4 +73,57 @@ class TestSetupColumns < MiniTest::Test
     end
   end
 
+  def column_array(table)
+    table.columns.map{ |c| c.local_name }
+  end
+
+  def test_columns
+    GrnMini::tmpdb do
+      array = GrnMini::Array.new
+      array.setup_columns(str: "")
+
+      assert_equal ["str"], column_array(Groonga["Array"])
+      assert_equal ["Array_str"], column_array(Groonga["Terms"])
+
+      array2 = GrnMini::Array.new("Array2")
+      array2.setup_columns(str: "", number: 1, float: 0.1, time: Time.at(0))
+
+      assert_equal ["float", "number", "str", "time"], column_array(Groonga["Array2"])
+      assert_equal ["Array2_str", "Array_str"], column_array(Groonga["Terms"])
+
+      # Change array schema
+      array.setup_columns(str: "", str2: "", number: 1)
+      assert_equal ["number", "str", "str2"], column_array(Groonga["Array"])
+      assert_equal ["Array2_str", "Array_str", "Array_str2"], column_array(Groonga["Terms"])
+    end
+  end
+
+  def test_multi_table_columns
+    GrnMini::tmpdb do
+      users = GrnMini::Hash.new("Users")
+      articles = GrnMini::Hash.new("Articles")
+
+      users.setup_columns(name: "", favorites: [articles])
+      articles.setup_columns(author: users, text: "")
+
+      users["aaa"] = {name: "Mr.A"}
+      users["bbb"] = {name: "Mr.B"}
+      users["ccc"] = {name: "Mr.C"}
+
+      articles["aaa:1"] = {author: "aaa", text: "111"}
+      articles["aaa:2"] = {author: "aaa", text: "222"}
+      articles["aaa:3"] = {author: "aaa", text: "333"}
+      articles["bbb:1"] = {author: "bbb", text: "111"}
+      articles["bbb:2"] = {author: "bbb", text: "222"}
+      articles["ccc:1"] = {author: "ccc", text: "111"}
+
+      users["aaa"].favorites = ["aaa:1", "bbb:2"]
+      users["bbb"].favorites = ["aaa:2"]
+      users["ccc"].favorites = ["aaa:1", "bbb:1", "ccc:1"]
+
+      assert_equal ["favorites", "name"], column_array(Groonga["Users"])
+      assert_equal ["Users_favorites", "author", "text"], column_array(Groonga["Articles"])
+      assert_equal ["Articles_text", "Users_name"], column_array(Groonga["Terms"])
+    end
+  end
 end
